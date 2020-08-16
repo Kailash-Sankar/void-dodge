1c2a66e706a5d09501ef99201c1e01df6171cc8c
Kailash Sankar <ksankar@conviva.com>
2020-08-16 22:36:34 +0530

    updated about page

--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -1,21 +1,46 @@
 import React from "react";
 import * as styles from "./index.module.less";
 
+function RefLink({ href, text }) {
+  return (
+    <a href={href} rel="noopener noreferrer" target="_blank">
+      {text}
+    </a>
+  );
+}
+
 function Story() {
   return (
     <div className={styles.about}>
       <div className={styles.sub}>
-        I am <a href={"https://kailash-sankar.github.io/#/"}>Kailash Sankar</a>,
-        a web developer.
+        I am{" "}
+        <RefLink
+          href={"https://kailash-sankar.github.io/#/"}
+          text="Kailash Sankar"
+        />
+        , a web developer.
         <br />
       </div>
       <div className={styles.sub}>
+        You can find the source code here{" "}
+        <RefLink
+          href={"https://github.com/Kailash-Sankar/void-dodge"}
+          text="void-dodge"
+        />
+      </div>
+      <div className={styles.sub}>
         Game icons are from{" "}
-        <a href="https://www.iconfinder.com/iconsets/space-82">IconFinder</a>
+        <RefLink
+          href={"https://www.iconfinder.com/iconsets/space-82"}
+          text="IconFinder"
+        />
       </div>
       <div className={styles.sub}>
         Music is from{" "}
-        <a href="https://www.fesliyanstudios.com/">fesliyanstudios</a>
+        <RefLink
+          href={"https://www.fesliyanstudios.com/"}
+          text="fesliyanstudios"
+        />
       </div>
     </div>
   );
